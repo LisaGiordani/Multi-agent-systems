b6a62d8de911303d6029afb3ab77c29d98116bbc
Lisa Giordani <72931093+LisaGiordani@users.noreply.github.com>
2023-05-26 19:34:58 +0200

    Delete Projet_final directory

--- Projet_final/fishnsharks/agents/__init__.py
+++
@@ -1,3 +0,0 @@
-from agents.fish import Fish
-from agents.shark import Shark
-from agents.seagull import Seagull
--- Projet_final/fishnsharks/env/__init__.py
+++
@@ -1,3 +0,0 @@
-from env.land import Land
-from env.sand import Sand
-from env.blood import Blood
